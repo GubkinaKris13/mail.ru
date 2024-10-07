22d5332b1d2d5ebacf00be65f063362274c0c1ae
GubkinaKris13 <christinagubkina@yandex.ru>
2024-10-08 00:08:46 +0300

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,17 @@
+# 1 st program
+print((9 ** 0.5) * 5)
+
+# 2 nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3 rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+
+# 4-я программа 
+name = "123,456"
+print(name [4])
+name = 123.456
+print(name * 10)
+print(int(name// 10))
